46e0014b651e7b0eabb3306203b2aafc3e247545
NKompa <nkompa7@gmail.com>
2022-10-16 14:32:46 +0300

    Sum of digits in number

--- 01_Sum_of_digits.py
+++ 01_Sum_of_digits.py
@@ -1,10 +1,13 @@
 # Программа принимает на вход вещественное число и показывает сумму его цифр: 6782 -> 23; 0,56 -> 11
 
 num = input('Введите число: ')
-num = num.replace('-', '')
-num = num.replace('.', '')
-num = num.replace(',', '')
-num = list(num)
+# 1 вариант удаления знаков
+for i in ['-', '.', ',']:
+    if i in num:
+        num = num.replace(i, "")
+
+# 2 вариант удаления знаков
+# num = num.translate({ord(i): None for i in '-.,'})
 
 sum = 0
 for i in num:
