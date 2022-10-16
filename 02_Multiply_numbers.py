97df5eb20472233087cd9709b91cc25150c71f24
NKompa <nkompa7@gmail.com>
2022-10-16 18:16:27 +0300

    Multiply numbers in range

--- 02_Multiply_numbers.py
+++ 02_Multiply_numbers.py
@@ -1,9 +1,9 @@
 # Программа принимает на вход число N и выдает набор произведений чисел от 1 до N: пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-number = int(input('Введите число: '))
+number = abs(int(input('Введите целое число: ')))
 mult = 1
 list = []
-for i in range(1,number):
+for i in range(1,number+1):
     mult *=i
     list.append(mult)
 print(list)
